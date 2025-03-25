2a3faec2708fe07ccc81fc29536ebf746b1f61e0
Stephen Walther <stephen.walther@superexpert.com>
2025-03-25 17:35:50 -0500

    Added Tavily

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { registerServerTool} from '@superexpert-ai/framework';
+import { tavily } from '@tavily/core';
+
+const tavilyClient = tavily({ apiKey: "tvly-YOUR_API_KEY" });
 
 
 registerServerTool({
   name: 'websearch',
   description: 'A tool for SuperExpert AI',
-  function() {
+  async function() {
+    const response = await tavilyClient.search("Who is Bertrand Russell?", { language: "en" });
+    console.log(response);
+
+
     return `hello from NPM package websearch-tool agent ${this.agent.name}`;
   }
 });
