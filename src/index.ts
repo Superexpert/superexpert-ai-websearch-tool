9a0fcfb31b57a1eb8834a2940831097500fad08c
Stephen Walther <stephen.walther@superexpert.com>
2025-03-25 18:05:34 -0500

    Added ServerDataTool

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { registerServerTool } from "@superexpert-ai/framework";
+import { registerServerDataTool, registerServerTool } from "@superexpert-ai/framework";
 import { tavily } from "@tavily/core";
 
 const tavilyClient = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 registerServerTool({
-  name: "websearch",
+  name: "webSearch",
   description: "Perform a web search,",
   parameters: [
     {
@@ -14,13 +14,36 @@ registerServerTool({
     },
   ],
   async function(query:string) {
-    const results = await tavilyClient.search(query, {
-      language: "en",
-    });
-    const textResult = results.results
-      .map((result) => `\nTitle: ${result.title}\nContent: ${result.content}\n`)
-      .join("\n");
-    console.log(textResult);
-    return textResult;
+    const results = await getWebSearchResults(query);
+    return results;
   },
 });
+
+
+registerServerDataTool({
+  name: "webSearchData",
+  description: "Perform a web search for each user message.",
+  async function() {
+    // Don't perform search if there is only one message
+    if (this.messages.length < 2) return;
+
+    // Get the last message from the user
+    const lastUserMessage = this.messages[this.messages.length - 1];
+    if (lastUserMessage.role !== "user") return;
+
+    // Perform a web search using the last user message as the query
+    const results = await getWebSearchResults(lastUserMessage.content);
+    return results;
+  }
+});
+
+async function getWebSearchResults(query: string) {
+  const results = await tavilyClient.search(query, {
+    language: "en",
+  });
+  const textResult = results.results
+    .map((result) => `\nTitle: ${result.title}\nURL: ${result.url}\nContent: ${result.content}\n`)
+    .join("\n");
+  console.log(textResult);
+  return textResult;
+}
