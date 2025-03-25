b86adce645c7d19e40e7fd480e92c783ba6e3542
Stephen Walther <stephen.walther@superexpert.com>
2025-03-25 17:56:38 -0500

    Added query parameter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import { registerServerTool} from '@superexpert-ai/framework';
-import { tavily } from '@tavily/core';
+import { registerServerTool } from "@superexpert-ai/framework";
+import { tavily } from "@tavily/core";
 
 const tavilyClient = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
-
 registerServerTool({
-  name: 'websearch',
-  description: 'A tool for SuperExpert AI',
-  async function() {
-
-    const results = await tavilyClient.search("Who is Bertrand Russell?", { language: "en" });
-    const textResult = results.results.map(result => `\nTitle: ${result.title}\nContent: ${result.content}\n`).join('\n');
+  name: "websearch",
+  description: "Perform a web search,",
+  parameters: [
+    {
+      name: "query",
+      type: "string",
+      description: "The search query to perform.",
+    },
+  ],
+  async function(query:string) {
+    const results = await tavilyClient.search(query, {
+      language: "en",
+    });
+    const textResult = results.results
+      .map((result) => `\nTitle: ${result.title}\nContent: ${result.content}\n`)
+      .join("\n");
     console.log(textResult);
     return textResult;
-  }
+  },
 });
